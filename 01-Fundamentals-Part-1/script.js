5ce76cbfbbd85f36dd303c518d69d3491a39c4c4
MkMonir <khanmonir1335@gmail.com>
2021-06-28 20:24:29 +0600

    basic operators

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -24,11 +24,53 @@ console.log(typeof 'monir');
 javscripIsFun = 'fun';
 console.log(typeof javscripIsFun);*/
 
-const birthDay = 1991;
+/*const birthDay = 1991;
 console.log(birthDay);
 //birthDay = 1992;
 //const Monir;
 var myJob = 'coding';
 myJob = 'true';
 console.log(typeof myJob);
-console.log(myJob); 
+console.log(myJob);       */
+
+
+
+///////math operators
+
+const now = 2020;
+const ageMonir = now - 2001;
+const ageAshik = now - 1995;
+console.log(ageMonir,ageAshik);
+
+console.log(ageMonir * 2, ageAshik / 5, 3 **3);
+
+const firstName = 'MOnir';
+const lastName = 'Khan';
+
+console.log(firstName + ' ' +lastName);
+let x = firstName + ' ' + lastName;
+console.log(x);               
+
+/////Assignment operators
+let m = 25+5;
+m += 10;
+m *=5;
+m++;
+m--;
+m--;
+console.log(m);
+
+//comparison operators
+console.log(ageMonir > ageAshik);
+console.log(ageMonir < ageAshik);
+console.log(ageMonir >= 19);
+console.log(ageMonir <= 10);            
+
+
+const now = 2020;
+const ageMonir = now - 2001;
+const ageAshik = now - 1995;
+
+const averageAge = (ageMonir + ageAshik) / 2;
+console.log(ageAshik,ageMonir,averageAge);
+
