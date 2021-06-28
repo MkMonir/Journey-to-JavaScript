ba525af2e5cbae92e802e0cc1c53487b4b83677a
MkMonir <khanmonir1335@gmail.com>
2021-06-28 18:27:51 +0600

    add 2 file

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,14 @@
+let js = "amazing";
+console.log(66+55-5+5);
+
+let firstName = "Monir";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let $myName = "Khan Shaheb";
+console.log($myName);
+let myFirstJob = 'Sales man';
+if (myFirstJob === 'Sales man') alert('Javascript is amazing');
