11ed004cc091bc5f942d85bf72104d0f282c870b
MkMonir <khanmonir1335@gmail.com>
2021-07-05 19:22:03 +0600

    Activating strict mode basic

---
+++ 02-Fundamentals-part-2/01-Activating-strict-mode-basic/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+// Activating Strict Mode
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio';
+// const private = 534;
