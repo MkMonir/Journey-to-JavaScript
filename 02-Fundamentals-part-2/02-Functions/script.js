b00d2efd78dad151e27d58d20dd53c8a0374d099
MkMonir <khanmonir1335@gmail.com>
2021-07-05 21:30:09 +0600

    Functions basic

---
+++ 02-Fundamentals-part-2/02-Functions/script.js
@@ -0,0 +1,27 @@
+'strict';
+
+function hello (){
+    console.log('My name is Monir');
+}
+
+//calling //running //invoking function
+
+hello();
+hello();
+hello();
+
+function fruitProcessor(apples,oranges){
+    console.log(apples,oranges);
+    const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5,0);
+console.log(appleJuice);
+console.log(fruitProcessor(5,0));
+
+const appleOrangeJuice = fruitProcessor(14,10);
+console.log(appleOrangeJuice); 
+
+const num = Number("45");
+console.log(num);
