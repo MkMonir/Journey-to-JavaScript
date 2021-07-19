b35016d14cc378d76deb2c8e8c9a996d1c6bab3f
MkMonir <khanmonir1335@gmail.com>
2021-07-19 13:20:07 +0600

    Iteration for the loop basic

---
+++ 02-Fundamentals-part-2/14-Iteration-The-for-Loop/script.js
@@ -0,0 +1,20 @@
+'strict';
+
+// console.log(`Lifting weights repitition  1🏋️‍♀️`);
+// console.log(`Lifting weights repitition  2🏋️‍♀️`);
+// console.log(`Lifting weights repitition  3🏋️‍♀️`);
+// console.log(`Lifting weights repitition  4🏋️‍♀️`);
+// console.log(`Lifting weights repitition  5🏋️‍♀️`);
+// console.log(`Lifting weights repitition  6🏋️‍♀️`);
+// console.log(`Lifting weights repitition  7🏋️‍♀️`);
+// console.log(`Lifting weights repitition  8🏋️‍♀️`);
+// console.log(`Lifting weights repitition  9🏋️‍♀️`);
+// console.log(`Lifting weights repitition  10🏋️‍♀️`);
+
+
+////For loop keeps running while condition is TRUE
+
+for(let rep = 1; rep <= 10; rep++){
+    console.log(`Lifting weights repitition ${rep} 🏋️‍♀️`);
+}
+
