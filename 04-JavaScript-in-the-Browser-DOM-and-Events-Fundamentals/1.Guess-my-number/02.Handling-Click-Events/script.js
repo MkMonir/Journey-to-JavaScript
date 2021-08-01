c82dc83f84b0e3b9673c39495591fac1dba8661b
MkMonir <khanmonir1335@gmail.com>
2021-08-01 17:36:18 +0600

    Click events

---
+++ 04-JavaScript-in-the-Browser-DOM-and-Events-Fundamentals/1.Guess-my-number/02.Handling-Click-Events/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number';
+  }
+});
