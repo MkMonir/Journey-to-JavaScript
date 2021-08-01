41078c92cb8f36ad98fee58fc8cd735ea0111b20
MkMonir <khanmonir1335@gmail.com>
2021-08-01 17:53:43 +0600

    Manipulating Css Syles

---
+++ 04-JavaScript-in-the-Browser-DOM-and-Events-Fundamentals/1.Guess-my-number/04.Manipulating-Css-Styles/script.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Answer!';
+    /////MANIPULATING CSS STYLES
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😖 You Lost The Game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '😖 You Lost The Game';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
