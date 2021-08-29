52ca1c9659e6fc5da3fe4cddc1e0cc0601dd977e
MkMonir <khanmonir1335@gmail.com>
2021-08-29 21:30:05 +0600

    Distructuring Arrays

---
+++ 06-Data-Structures-Mordern-Operators-And-Strings/1.Destructuring-Arrays/script.js
@@ -0,0 +1,45 @@
+'use strict';
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+};
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+////SWITCHING VARIABLES
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+const [starter, mainCourse] = restaurant.order(3, 1);
+console.log(starter, mainCourse);
+
+///NESTED DISTRUCTURING
+const nested = [2, 3, [4, 5]];
+// const [m, , l] = nested;
+const [m, , [k, j]] = nested;
+console.log(m, k, j);
+
+const [l = 1, n = 5, g = 9] = [8];
+console.log(l, n, g);
