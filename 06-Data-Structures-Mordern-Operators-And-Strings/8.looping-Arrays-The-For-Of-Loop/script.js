0ec1111cdd3086abbd8b718c14cb7b3497046cc4
MkMonir <khanmonir1335@gmail.com>
2021-09-12 00:21:26 +0600

    Looping arrays with the for of loop

---
+++ 06-Data-Structures-Mordern-Operators-And-Strings/8.looping-Arrays-The-For-Of-Loop/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here your pasta with ${ing1} ${ing2} and ${ing3}`);
+  },
+  orderPizza: function (mainIngredient, ...otherIngredient) {
+    console.log(mainIngredient);
+    console.log(otherIngredient);
+  },
+};
+
+const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+
+for (const item of menu) console.log(item);
+
+for (const [i, el] of menu.entries()) console.log(`${i + 1} : ${el}`);
