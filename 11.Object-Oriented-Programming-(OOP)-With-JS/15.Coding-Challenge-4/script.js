f3af22e02c3e57c67bc8ca25daddd99c70a991fb
MkMonir <khanmonir1335@gmail.com>
2021-11-12 15:07:42 +0600

    Coding challenge

---
+++ 11.Object-Oriented-Programming-(OOP)-With-JS/15.Coding-Challenge-4/script.js
@@ -0,0 +1,64 @@
+'use strict';
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} going at ${this.speed}km/h`);
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} going at ${this.speed}km/h`);
+    return this;
+  }
+
+  get sppedUs() {
+    return this.speed / 1.6;
+  }
+
+  set sppedUs(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+class EVCl extends CarCl {
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+    console.log(
+      `${this.make} is going at ${this.speed}km/h, with a charge ${
+        this.#charge
+      }%`
+    );
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+console.log(rivian);
+rivian
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .chargeBattery(38)
+  .accelerate();
+
+console.log(rivian.sppedUs);
